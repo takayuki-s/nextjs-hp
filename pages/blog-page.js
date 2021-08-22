51555867970535b9e492c90855776268d4419f25
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-08-23 06:26:41 +0900

    APIで取得したデータをページに表示

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,11 +1,22 @@
 import { Layout } from "../components/Layout"
+import Post from "../components/Post";
+import { getAllPostsData } from "../lib/posts";
 
-const Blog = () => {
+const Blog = ({ posts }) => {
   return (
     <Layout title="Blog">
-      Blog page
+      <ur className="m-10">
+        {posts && posts.map((post) => <Post key={post.id} post={post} />)}
+      </ur>
     </Layout>
   )
 }
 
-export default Blog
+export default Blog;
+
+export async function getStaticProps() {
+  const posts = await getAllPostsData();
+  return {
+    props: { posts }
+  }
+}
